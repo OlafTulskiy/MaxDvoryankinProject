ddba6f82cf63e10d090c9e2d31d805956f4fce99
m.dvoryankin <m.dvoryankin@npo-pkrv.ru>
2022-04-13 18:09:18 +0300

    Add one comment

--- src/main/java/ru/geekbrains/lessons1/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/lessons1/HomeWorkApp.java
@@ -2,11 +2,16 @@ package ru.geekbrains.lessons1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        //lesson one---
+        //printThreeWords();
+        //checkSumSign();
+        //printColor();
+        //compareNumbers();
         //lesson one done
+
+        //lesson two ---
+        checkSumm(1,5);
+        checkNumber(11);
     }
 
     public static void printThreeWords() {
@@ -47,4 +52,40 @@ public class HomeWorkApp {
         else
             System.out.println(" a < b ");
     }
+    // lesson 2
+    public static boolean checkSumm(int numberOne, int numberTwo) {
+        if ((numberOne+numberTwo) >= 10 && (numberOne+numberTwo) <= 20)
+            return true;
+        else
+            return false;
+    }
+    public static void checkNumber(int numberOne) {
+        if (numberOne >= 0)
+            System.out.println("Number is positive");
+        else
+            System.out.println("Number is negative");
+    }
+
+    public static boolean isNegativeNumber (int numberOne) {
+        if (numberOne >= 0)
+            return false;
+        else
+            return true;
+    }
+
+    public static void printString (String someString, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(someString);
+        }
+    }
+
+    public static boolean findDeathYear (int year) {
+        boolean tmp = false;
+        if ((year % 4 == 0) || year % 400 == 0) {
+            if (year % 100 != 0)
+                tmp = true;
+        }
+        return tmp;
+    }
+
 }
